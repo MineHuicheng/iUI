c8aada96646a54397ef8248224c9cce301786be6
Minehuicheng <zhengguolei_mine@163.com>
2020-03-23 23:17:39 +0800

    feat: update page

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": "error"
+    "prettier/prettier": "off"
   }
 };
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts"> //scrpit中需要指明lang=ts，否则编辑器将不能识别语言类型而报错
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+  @Prop() private msg: string = 'Welcome to Your Vue.js App';
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
